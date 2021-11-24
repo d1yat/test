662f85a79f5c3a0aa1f7f8e0394d8eb7dcbcd3b6
d1yat <me.sudiyat@gmail.com>
2021-11-24 09:16:05 +0700

    add 'php' files

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 
-class Person
-{
-	public $id;
-	public $name;
-
-	function __construct($id, $name)
-	{
-		$this->id = $id;
-		$this->name = $name;
-	}
-}
+require_once 'person.php';
 
 class Main
 {
 	public static function index()
 	{
-		echo 'Hey now brown cow';
+		$users = [
+			new Person(101, 'Ahmad Fasehan'),
+			new Person(102, 'Andris Firmansyah'),
+			new Person(103, 'Akhirudin Sudiyat'),
+			new Person(104, 'Bani Arham'),
+			new Person(105, 'Rido Raudho')
+		];
+
+		foreach ($users as $user) {
+			echo $user->name . "\n";
+		}
 	}
 }
 
