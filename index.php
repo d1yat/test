b01b8fa3007ddd4e487cbcfea01e5a8aeb5d086d
Akhirudin Sudiyat <me.sudiyat@gmail.com>
2024-01-29 21:59:21 +0700

    change to for loop

--- index.php
+++ index.php
@@ -17,10 +17,16 @@ class Main
 			new Person(104, 'Bani Arham'),
 			new Person(105, 'Rido Raudho')
 		];
-
-		foreach ($users as $user) {
+		
+		$size = count($users);
+		for ($i = 0; $i < $size; $i++) {
+			$user = $users[$i];
 			echo "({$user->id}) => {$user->name}\n";
 		}
+
+		// foreach ($users as $user) {
+			// echo "({$user->id}) => {$user->name}\n";
+		// }
 	}
 }
 
